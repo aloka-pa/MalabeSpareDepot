16a2de533264b333b33121dc7db8c8b4d8df8db3
Aloka Pathiraja <aloka.pa@peopleshr.com>
2026-07-12 00:09:16 +0530

    Add inventory array and display multiple parts

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,51 @@
 public class Main {
 
     public static void main(String[] args) {
-		Part piston = new Part(
-        "P001",
-        "Bajaj Piston",
-        "Bajaj",
-        4500,
-        15,
-        "Engine");
-		
-		System.out.println("Code: " + piston.getCode());
-System.out.println("Name: " + piston.getName());
-System.out.println("Brand: " + piston.getBrand());
-System.out.println("Price: " + piston.getPrice());
-System.out.println("Quantity: " + piston.getQuantity());
-System.out.println("Category: " + piston.getCategory());
-    }
+        Part[] inventory = new Part[3];
 
+        inventory[0] = new Part(
+                "P001",
+                "Bajaj Piston",
+                "Bajaj",
+                4500,
+                15,
+                "Engine"
+        );
+
+        inventory[1] = new Part(
+                "P002",
+                "Brake Pad",
+                "TVS",
+                1250,
+                8,
+                "Brakes"
+        );
+
+        inventory[2] = new Part(
+                "P003",
+                "Spark Plug",
+                "NGK",
+                850,
+                50,
+                "Electrical"
+        );
+
+        System.out.println("========= INVENTORY =========");
+        System.out.println();
+
+        for (int i = 0; i < inventory.length; i++) {
+
+            System.out.println("Code: " + inventory[i].getCode());
+            System.out.println("Name: " + inventory[i].getName());
+            System.out.println("Brand: " + inventory[i].getBrand());
+            System.out.println("Price: " + inventory[i].getPrice());
+            System.out.println("Quantity: " + inventory[i].getQuantity());
+            System.out.println("Category: " + inventory[i].getCategory());
+
+            System.out.println("----------------------------");
+            System.out.println();
+        }
+
+        System.out.println("Total Parts: " + inventory.length);
+    }
 }
